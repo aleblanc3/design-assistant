b485996b62f70f3e156a9157329947bd0529410e
Amber <57812460+aleblanc3@users.noreply.github.com>
2026-08-13 23:33:57 -0400

    Extract view pages to it's own reusable component

---
+++ src/app/components/view-pages/view-pages.component.ts
@@ -0,0 +1,65 @@
+import { Component, inject, computed } from '@angular/core';
+import { TranslatePipe } from "@ngx-translate/core";
+import { RouterLink } from '@angular/router';
+
+//PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+
+//Custom
+import { ProjectSettingsComponent } from '../project-settings/project-settings.component';
+import { ProjectStateService } from '../../services/project-state.service';
+import { ProjectCacheService } from '../../services/project-cache.service';
+import { IaDiagramService } from '../ia-diagram/ia-diagram.service';
+
+@Component({
+    selector: 'aida-view-pages',
+    imports: [
+        TranslatePipe, RouterLink,
+        ButtonModule, DrawerModule, TableModule, TooltipModule,
+        ProjectSettingsComponent
+    ],
+    templateUrl: './view-pages.component.html',
+    styles: ``
+})
+export class ViewPagesComponent {
+    protected projectState = inject(ProjectStateService)
+    protected projectCache = inject(ProjectCacheService)
+    protected iaDiagram = inject(IaDiagramService)
+
+//UI elements
+  inScopePageCount = computed(() => this.projectState.getProject().inScopePages);
+  showUrls = false;
+  showIA = false;
+  showBreadcrumb = false;
+
+  //URL drawer - Both languages
+  pairedPagesForTable = computed(() =>
+    this.projectState.getPairedPages(this.projectCache.selectedSource(), this.projectCache.selectedScope())
+  );
+
+  //URL drawer - One language
+  singlePagesForList = computed(() => {
+    const selectedLang = this.projectCache.selectedLang();
+    const lang = selectedLang === 'both' ? this.projectState.detectPrimaryLanguage() : selectedLang;
+    return this.projectState.getAllPages(lang, this.projectCache.selectedSource(), this.projectCache.selectedScope());
+  });
+
+  //URL drawer - Copy
+  async copyToClipboard(lang: 'en' | 'fr' | 'both'): Promise<void> {
+    const pairs = this.pairedPagesForTable();
+    let text;
+    if (lang === 'both') {
+      text = pairs.map(pair => `${pair.en.url}\t${pair.fr.url}`).join('\r\n');
+    } else {
+      text = pairs.map(pair => `${pair[lang].url}`).join('\r\n');
+    }
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (error) {
+      console.error('Failed to copy to clipboard:', error);
+    }
+  }
+}
